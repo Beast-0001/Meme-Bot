32a6a5872c7abb2791b6d93c25b9dfd779406a7b
thebeastatelectricity <56598575+thebeastatelectricity@users.noreply.github.com>
2020-08-27 08:35:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ const client = new Discord.Client();
 client.on('ready',() => {
 console.log(`Logged in as ${client.user.tag}`);
 const channel = client.channels.cache.get('746305375082512474');
+    let meme = await fetch('https://memes.blademaker.tv/api/ ');
+    let q = await meme.text()
+    q = JSON.parse(q)   
+    channel.send(new Discord.MessageEmbed().setTitle(q.title).setAuthor(q.author).setImage(q.image).setFooter(`${q.ups} 👍 | ${q.downs} 👎`))
   setInterval(async () => {
     let meme = await fetch('https://memes.blademaker.tv/api/ ');
-     meme = meme.json();
-     channel.send(
-     new Discord.MessageEmbed()
-      .setTitle('A Meme!')
-      .setDescription(meme.title)
-      .setImage(meme.url)
-      .setFooter(`${client.user.username} | Bot by: Beast#0505`, client.user.displayAvatarURL())
-)
+    let q = await meme.text()
+    q = JSON.parse(q)   
+    channel.send(new Discord.MessageEmbed().setTitle(q.title).setAuthor(q.author).setImage(q.image).setFooter(`${q.ups} 👍 | ${q.downs} 👎`))
   }, 216000);
 });
 
