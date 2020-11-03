6bc53d63e134a57dc5c86721865ea9eb6e77b5c0
Beast <56598575+Beast-0001@users.noreply.github.com>
2020-11-02 21:28:59 -0500

    Formatted stuff!

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const Discord = require('discord.js');
-const fetch = require('node-fetch');
-const config = require('./config.js');
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+const config = require("./config.js");
 const client = new Discord.Client();
 
-client.on('ready', async() => {
-console.log(`Logged in as ${client.user.tag}`);
-const channel = client.channels.cache.get(config.channelID);
-    let meme = await fetch('https://memes.blademaker.tv/api/ ');
+client.on("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    const channel = client.channels.cache.get(config.channelID);
+    let meme = await fetch("https://memes.blademaker.tv/api/ ");
     let q = await meme.text();
     q = JSON.parse(q);
     channel.send(new Discord.MessageEmbed().setTitle(q.title).setAuthor(q.author).setImage(q.image).setFooter(`${q.ups} 👍 | ${q.downs} 👎`));
-  setInterval(async () => {
-    let meme = await fetch('https://memes.blademaker.tv/api/ ');
-    let q = await meme.text();
-    q = JSON.parse(q);
-    channel.send(new Discord.MessageEmbed().setTitle(q.title).setAuthor(q.author).setImage(q.image).setFooter(`${q.ups} 👍 | ${q.downs} 👎`));
-  }, 216000);
+    setInterval(async () => {
+        let meme = await fetch("https://memes.blademaker.tv/api/ ");
+        let q = await meme.text();
+        q = JSON.parse(q);
+        channel.send(new Discord.MessageEmbed().setTitle(q.title).setAuthor(q.author).setImage(q.image).setFooter(`${q.ups} 👍 | ${q.downs} 👎`));
+    }, 10800000);
 });
 
 client.login(config.token);
